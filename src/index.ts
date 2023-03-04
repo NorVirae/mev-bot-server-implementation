ec656d574516e934d69eb2d8e0a92c40c68f82ea
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-03-04 15:50:06 +0100

    remove env

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,8 @@ const BUSD_TEST = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
 const CAKE_TEST = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"
 const DOT_TEST = "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402"
 
+// smart contract address
+
 async function main() {
 
   // Wallets and providers for wallets and contracts ~
